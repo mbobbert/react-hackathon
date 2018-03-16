4576debde26b1ab3b7165b2a37b6aa6c224beb4a
Unknown <zerayah@gmail.com>
2018-03-16 16:37:42 +0100

    even more styling

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ class App extends React.Component {
 	render() {
 		return (
 			<div>
-				<h1>To Do List</h1>
+				<h1>Quest List</h1>
 				<div className="container">
 					<TaskForm />
 					<TaskList />
